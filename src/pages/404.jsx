cddbf3c0cd3e8c9763032c0f609efe5bff2fc8db
thorsten zerha <zet@flinkwork.com>
2019-10-25 12:19:39 +0200

    up

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import LogoImg from '../images/logo.png'
 import '../sass/theme-styles.scss'
 
 // import Layout from '../components/Layout'
@@ -11,38 +12,22 @@ export default class Index extends Component {
       <div>
         <section className="page404">
           <div className="site-logo logo--center">
-            <a href="index.html" className="full-block">H</a>
-            <img src="img/logo-white.png" alt="Utouch" />
+            <a href="https://ticketteer.com" className="full-block">
+              <img src={LogoImg} alt="ticketteer logo" />
+            </a>
               <div className="logo-text">
                 <div className="logo-title">ticketteer</div>
                 <div className="logo-sub-title">sell more tickets</div>
               </div>
 	          </div>
-            <div className="col-8 bg-secondary-color"></div>
-            <div className="col-8 bg-primary-color"></div>
-            <div className="col-8 bg-blue-light">
-              <h2 className="error">Error</h2>
-            </div>
-            <div className="col-8 bg-orange-light">
-              <h2 className="number">4</h2>
-            </div>
-            <div className="col-8 bg-red">
-              <h2 className="number">0</h2>
-            </div>
-            <div className="col-8 bg-green">
-              <h2 className="number">4</h2>
-            </div>
-            <div className="col-8 bg-secondary-color"></div>
-            <div className="col-8 bg-primary-color"></div>
-
-            <div className="page404-content">
+            <div className="page404-content pt-5">
               <h2 className="title">Sorry! The Page Not Found ;(</h2>
               <h5 className="sub-title">We can't find the page you are looking for. Please go to <a href="index.html">Homepage</a>.</h5>
             </div>
 
             <div className="sub-footer">
               <span>
-                Copyright © 2017 <a href="index.html" className="sub-footer__link">ticketteer</a>
+                Copyright © 2019 <a href="index.html" className="sub-footer__link">ticketteer</a>
               </span>
             </div>
         </section>
