a4a841ee403af013baead55013e6735134e696fa
Thorsten Zerha <zet@entrello.io>
2024-04-09 21:21:05 +0200

    redirect to entrello

--- src/utils/colors.js
+++
@@ -1,9 +0,0 @@
-
-const colorClassBGCodes = {
-  'blue': 'bg-5',
-  'orange-light': 'bg-6',
-  'orange': 'bg-o',
-  'red': 'bg-7',
-}
-
-export const getBGClass = colorClass => colorClassBGCodes[colorClass]
